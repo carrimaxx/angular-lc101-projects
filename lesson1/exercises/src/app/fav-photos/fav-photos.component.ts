ce73defb98a312d8833dda27e90bcc2f826c79b2
Maricar Walters <walterskai7@gmail.com>
2020-10-26 15:46:43 -0500

    modified movie-list, fav-photos, and chores-list

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'My Favorite Photos';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://img.freepik.com/free-vector/flat-paper-coffee-cup_23-2147739201.jpg?size=338&ext=jpg';
+  image3 = 'https://img.freepik.com/free-vector/cakes-icons-set_1284-14668.jpg?size=338&ext=jpg';
 
   constructor() { }
 
